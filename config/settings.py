bf71c3e19dace3a5714b6b15f1b64711fad2d4d2
Abhishek Tiwari <abhie.lp@gmail.com>
2019-05-26 09:57:21 +0530

    Added mailinglist app

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "mailinglist.apps.MailinglistConfig",
 ]
 
 MIDDLEWARE = [
