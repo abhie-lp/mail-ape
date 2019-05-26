6515746f733b50ded659882ad7a2e5353cf7de75
Abhishek Tiwari <abhie.lp@gmail.com>
2019-05-26 12:45:08 +0530

    Added MailingList create view

--- mailinglist/urls.py
+++ mailinglist/urls.py
@@ -1,8 +1,13 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 app_name = "mailinglist"
 
+mailinglist_urls = [
+    path("new/", views.MailingListCreateView.as_view(), name="create"),
+]
+
 urlpatterns = [
+    path("mailinglist/", include(mailinglist_urls)),
     path("", views.MailingListListView.as_view(), name="list"),
 ]
--- mailinglist/views.py
+++ mailinglist/views.py
@@ -1,4 +1,4 @@
-from . import models, mixins
+from . import models, mixins, forms
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -7,3 +7,12 @@ class MailingListListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return models.MailingList.objects.filter(owner=self.request.user)
+
+
+class MailingListCreateView(LoginRequiredMixin, generic.CreateView):
+    form_class = forms.MailingListForm
+    template_name = "mailinglist/mailinglist_form.html"
+
+    def get_initial(self):
+
+        return {"owner": self.request.user.id,}
